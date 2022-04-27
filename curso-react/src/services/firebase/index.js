5052a56d8a266aa46fb2c8e81dd285be14809b11
Santiago Moyano <santiagogmoyano@gmail.com>
2022-04-26 21:57:53 -0300

    .env

--- curso-react/src/services/firebase/index.js
+++ curso-react/src/services/firebase/index.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD06noTTFfBq5bD7byvVMF14A6cW6BZSMU",
-  authDomain: "backendsantiagomoyano.firebaseapp.com",
-  projectId: "backendsantiagomoyano",
-  storageBucket: "backendsantiagomoyano.appspot.com",
-  messagingSenderId: "1068066348406",
-  appId: "1:1068066348406:web:5c11fd3948e2d240136e57"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId 
 };
 
 const app = initializeApp(firebaseConfig);
